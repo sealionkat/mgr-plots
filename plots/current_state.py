b46459918473f7d1d364f25e7680c4b8f7be7e43
sealionkat <sealionkat@users.noreply.github.com>
2018-08-05 13:30:17 +0200

    Added handling argv.

--- plots/current_state.py
+++ plots/current_state.py
@@ -2,22 +2,31 @@ import matplotlib.pyplot as plt
 import sys
 import os
 
-arguments = sys.argv
-print(arguments)
 
-print('current script:', sys.argv[0])
+def main():
+  arguments = sys.argv
+  print(arguments)
 
-if arguments[1] == '-d':
-  print('analyze directory', arguments[1])
-  path = arguments[2]
-  try:
-    os.chdir(path)
-    print('path changed to:', path)
-  except FileNotFoundError:
-    print('bad path!')
-    sys.exit()
-else:
-  print('bad argument!')
+  if len(arguments) < 2:
+    print('Not enough arguments')
+    exit(1)
+
+  print('current script:', sys.argv[0])
+
+  if arguments[1] == '-d':
+    print('analyze directory', arguments[1])
+    path = arguments[2]
+    try:
+      os.chdir(path)
+      print('path changed to:', path)
+    except FileNotFoundError:
+      print('bad path!')
+      sys.exit()
+  else:
+    print('bad argument!')
+
+if __name__ == '__main__':
+  main()
 
 
 
